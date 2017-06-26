143390ed5e54f4434f99524df2af730e2d98af88
Aldrin Piri <aldrinpiri@gmail.com>
2017-06-26 00:06:53 -0400

    Only look for certificate folders owned by the user we are ssh'd as.

--- recreate-keychain-and-import.sh
+++ recreate-keychain-and-import.sh
@@ -25,7 +25,7 @@ if [ "${docker_host}" = 'localhost' ]; then
     forwarded_port=$(docker port unsecured_nifi-node_1 | grep 8443 | cut -d':' -f 2)
 else
   ssh -t "${docker_host}" < generate_certificates.sh
-  remote_cert_directory=$(ssh "${docker_host}" "find /tmp -maxdepth 1 -type d -name '*nifi-docker-certs*' | sort -r | head -n 1")
+  remote_cert_directory=$(ssh "${docker_host}" 'find /tmp -maxdepth 1 -type d -user $(whoami) -name "*nifi-docker-certs*" | sort -r | head -n 1')
   echo "Generated certificates are available at ${remote_cert_directory}"
   rsync -r "${docker_host}":"${remote_cert_directory}"/ "${cert_path_base}"/
   echo "Rsync completed to ${cert_path_base}"
